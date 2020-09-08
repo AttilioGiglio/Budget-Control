9f52ba6914d9288da635f33133cc4e1a62b0e305
attiliogiglio <attilio.g93@gmail.com>
2020-09-08 14:42:54 -0300

    budget input validation

--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -1,21 +1,35 @@
 import React,{Fragment,useState} from 'react';
+import Error from './Error';
+
 
 const Questions = () => {
     // define State
     const [amount, setAmount] = useState(0);
+    const [error, stateError] = useState(false);
     
     // Function to read budget defined
     const readBudget = (e) => {
-       setAmount(parseInt(e.target.value, 10))
+       setAmount(parseInt(e.target.value))
     }
 
     // Function to add budget defined
     const addBudget = (e) => {
+        e.preventDefault();
+
+        // Form validation
+        if(amount<1){
+            setError(true);
+            return;
+        }
+        // passed validation
+        setError(false);
+
      }
 
     return (
         <Fragment>
             <h2>Questions</h2>
+            {error ? <Error /> : null}
             <form onSubmit={addBudget}>
                 <input 
                 type='number'
