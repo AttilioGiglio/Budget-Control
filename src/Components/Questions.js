dc0623e9498b4b27b629fbeac0627316e093836c
attiliogiglio <attilio.g93@gmail.com>
2020-09-08 12:56:01 -0300

    adding budget state

--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -1,14 +1,27 @@
-import React,{Fragment} from 'react'
+import React,{Fragment,useState} from 'react';
 
 const Questions = () => {
+    // define State
+    const [amount, setAmount] = useState(0);
+    
+    // Function to read budget defined
+    const readBudget = (e) => {
+       setAmount(parseInt(e.target.value, 10))
+    }
+
+    // Function to add budget defined
+    const addBudget = (e) => {
+     }
+
     return (
         <Fragment>
             <h2>Questions</h2>
-            <form>
+            <form onSubmit={addBudget}>
                 <input 
                 type='number'
                 className='u-full-width'
                 placeholder='Coloca tu presupuesto'
+                onChange={readBudget}
                 />
                 <input 
                 type='submit'
