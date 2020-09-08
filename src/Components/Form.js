c48b09c311ab2fa38a3ecc4d21857634bbb6ebce
attiliogiglio <attilio.g93@gmail.com>
2020-09-08 16:35:54 -0300

    validate expense form

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,17 +1,26 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import Error from './Error';
 
 const Form = () => {
     const[name, setName] = useState('');
     const[number, setNumber] = useState(0);
-
+    const[error, setError] = useState(false);
+    // when the use add an expense
     const addExpenses = (e) =>{
         e.preventDefault();
-        console.log(name,number)
+        
+        // validation
+        if(number < 1 || isNaN() || name.trim() === ''){
+            setError(true);
+            return;
+        }
+        setError(false);
     } 
 
     return (
         <form onSubmit={addExpenses}>
             <h2>Add your expenses</h2>
+            {error ? <Error mensaje='Both inputs are required' /> : null }
             <div className='campo'>
                 <label>Expense Name</label>
                 <input 
