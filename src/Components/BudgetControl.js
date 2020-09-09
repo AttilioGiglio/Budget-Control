e5d9f67c4720f9e57e6601d91ce3cba76231cb5a
attiliogiglio <attilio.g93@gmail.com>
2020-09-09 17:54:40 -0300

    added dynamic css to budget control

--- src/Components/BudgetControl.js
+++ src/Components/BudgetControl.js
@@ -1,4 +1,5 @@
 import React, {Fragment} from 'react';
+import {budgetChecking} from '../Components/Helper'
 
 const BudgetControl = ({budget,rest}) => {
     return (
@@ -6,7 +7,7 @@ const BudgetControl = ({budget,rest}) => {
             <div className='alert alert-primary'>
                 Presupuesto: ${budget}
             </div>
-            <div className='alert'>
+            <div className={budgetChecking(budget,rest)}>
                 Restante: ${rest}
             </div>
         </Fragment>
